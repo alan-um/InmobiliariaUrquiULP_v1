e5448b640c5cddc9bca0a41eafd99167e997bc9d
alan-um <urquizamanzanelli.aj@gmail.com>
2025-09-18 20:55:32 -0300

    Se creó MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InmobiliariaUrquiULP_v1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
